6ec42b73fbfb8512ac4308f296577a1d2b3d5548
Nzekwe Isaac Edidiong <curlyzik@gmail.com>
2022-04-06 01:43:41 +0100

    deploy first contract

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -8,16 +8,20 @@ const provider = ganache.provider();
 const web3 = new Web3(provider);
 
 let accounts;
+let inbox;
 
 beforeEach(async () => {
   // get a list of all accounts
   accounts = await web3.eth.getAccounts();
 
   // use one of those accounts to deploy the contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ["Hi there!"] })
+    .send({ from: accounts[0], gas: "1000000" });
 });
 
 describe("Inbox", () => {
   it("deploys a contract", () => {
-    console.log(interface);
+    console.log(inbox);
   });
 });
